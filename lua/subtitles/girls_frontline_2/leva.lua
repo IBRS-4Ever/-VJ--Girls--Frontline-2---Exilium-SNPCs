e366d22b6caf3d6bdace60042dfcfe1b9a458461
IBRS <56501720+IBRS-4Ever@users.noreply.github.com>
2025-03-29 16:07:57 +0800

    Added Leva's Subtitles.

--- lua/subtitles/girls_frontline_2/leva.lua
+++ lua/subtitles/girls_frontline_2/leva.lua
@@ -10,12 +10,122 @@ local CharacterColor = Color(230,190,70)
 local GF2Subtitle = {
 
 	{
+		snd = "vo/jp/leva/alert1.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "准备好了~",
+			["english"] = "I'm ready~",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/alert2.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "忍一下吧。",
+			["english"] = "Come on, come on.",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/alert3.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "珍惜还活着的时候吧。",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/alert4.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "请保持安静。",
+			["english"] = "Be quiet.",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/alert5.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "痛的话，就叫出来吧。",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/alert6.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "还有一些小惊喜♪",
+			["english"] = "Surprise~",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/die1.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "至少，我们知道下次怎么做才不会这么狼狈了，对吧？",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/die2.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "不能在这倒……下……",
+			["english"] = "Can't stop... here...",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/die3.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "糟糕……",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
 		snd = "vo/jp/leva/follow1.wav",
 		subject = CharacterName,
 		text = { -- ID: 
-			["schinese"] = "我在这里。", 
-			["english"] = "I'm here.", 
-			--["japanese"] = "白梟の声だよ～、えへへ♪"
+			["schinese"] = "我就在这里。",
+			["english"] = "I'm here.",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/follow2.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "需要我吗？",
+			["english"] = "You need me?",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/follow3.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "我在听哦。",
+			["english"] = "I'm listening.",
+			--["japanese"] = " ",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -24,9 +134,273 @@ local GF2Subtitle = {
 		snd = "vo/jp/leva/follow4.wav",
 		subject = CharacterName,
 		text = { -- ID: 
-			["schinese"] = "指令是？", 
-			["english"] = "The instructions are...?", 
-			--["japanese"] = "白梟の声だよ～、えへへ♪"
+			["schinese"] = "指令是？",
+			["english"] = "The instructions are...?",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/follow5.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "接下来，我们的合作将会更加紧密哟，期不期待呀，指挥官？",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/get_heal1.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "嗯？认真的吗？",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/get_heal2.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "不错不错？",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/get_heal3.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "还要继续~？",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/get_heal4.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "感觉不一样了呢。",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/get_heal5.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "想依靠我也可以哦。",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/hit6.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "有点不妙……",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/idle1.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "唔哇~被吓了一跳呢，完全没注意到指挥官——嗯？太假了？哈哈，抱歉抱歉，下次我会装得更像一点的。",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/idle2.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "指挥官，可以再靠近一点哦，怎么，难道是在担心自己的鼻子吗~？",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/idle3.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "每次看到可露凯那么努力的样子，就会忍不住想要欺负一下呢……啊，不用替我保密也可以哟，指挥官。",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/idle4.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "对于指挥官，我知无不言，但不包括你没问到的事哦。所以——请尽情地思考一下，我还可能知道些什么吧~",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/idle5.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "所谓“家人”就是要共享秘密、相互扶持、庇佑——嗯……还是给彼此留一些空间会比较好吧？但如果指挥官想让我知道你的全部，我也不介意哦~？",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/killed_enemy1.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "想知道自己运气如何吗？",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/killed_enemy2.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "不再见咯。",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/killed_enemy3.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "我的表现不错？那是因为指挥官调度得当嘛~",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/player_sight1.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "果然，无论外表、身份、立场再怎么变化，指挥官依然还是指挥官。",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/player_sight2.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "这点小事交给我吧，顺手就可以解决……麻烦？不会哦，毕竟这是来自家人的请求嘛~",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/player_sight3.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "喜欢的东西和不喜欢的东西？诶——暴露自己的喜好可是一件很危险的事哦，不过如果是指挥官想知道，我可以考虑一下告诉你~",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/player_sight4.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "捉摸不透？在说我吗？哼哼，这是不是说明……指挥官经常在想着我的事呢？",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/player_sight5.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "怎么了指挥官，黑眼圈有点重呢。晚上睡不着的话，要我陪你聊聊天吗？说不定聊着聊着就困了哦~",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/player_sight6.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "人形的皮肤，很柔软吧？唔……不过比起人类，可能还是差了一点，指挥官也凑近一点，让我戳戳看？只是比较一下啦~",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/player_sight7.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "其实，“家人”只算是一个笼统的称呼，家人之间的关系也还是有各种各样的细分呢。那么对于指挥官而言，我究竟是什么呢？仔细地、慢慢地考虑一下吧~",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/player_sight8.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "指挥官，在所有纷扰尘埃落定之后，让我们分享彼此以后所有的时间，好吗？这是个约定，答应了就不能反悔了哦。",
+			--["english"] = " ",
+			--["japanese"] = " ",
+		},
+		subjectcol = CharacterColor,
+	},
+
+	{
+		snd = "vo/jp/leva/player_sight9.wav",
+		subject = CharacterName,
+		text = { -- ID: 
+			["schinese"] = "哎呀，是指挥官~怎么样，有想我吗？",
+			--["english"] = " ",
+			--["japanese"] = " ",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -35,9 +409,9 @@ local GF2Subtitle = {
 		snd = "vo/jp/leva/unfollow1.wav",
 		subject = CharacterName,
 		text = { -- ID: 
-			["schinese"] = "了解。", 
+			["schinese"] = "了解——", 
 			["english"] = "OK!", 
-			--["japanese"] = "白梟の声だよ～、えへへ♪"
+			--["japanese"] = " ",
 		},
 		subjectcol = CharacterColor,
 	},
