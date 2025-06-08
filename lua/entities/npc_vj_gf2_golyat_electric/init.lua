f49f8e4a413d5f1debbc7f9480547e6b70150b48
IBRS <56501720+IBRS-4Ever@users.noreply.github.com>
2025-06-08 22:51:05 +0800

    Updated Florence's Physics.

--- lua/entities/npc_vj_gf2_golyat_electric/init.lua
+++ lua/entities/npc_vj_gf2_golyat_electric/init.lua
@@ -1,7 +1,22 @@
 AddCSLuaFile("shared.lua")
 include('shared.lua')
 
+--ENT.HasDeathCorpse = true
+
 ENT.MeleeAttackDamage = 250
 ENT.MeleeAttackDamageType = DMG_SHOCK
+--ENT.DisableDefaultMeleeAttackDamageCode = true
+--ENT.TimeUntilMeleeAttackDamage = false
 
 ENT.GolyatSkin = 1
+--[[ 
+ENT.CustomAttackCheck_MeleeAttack = false
+
+function ENT:OnMeleeAttackExecute(status, ent, isProp)
+	if status == "PreInit" then
+		print("asasdasdasdad")
+		VJ.ApplyRadiusDamage(self, self, self:GetPos(), self.MeleeAttackDamageDistance, self.MeleeAttackDamage, self.MeleeAttackDamageType, !GetConVar("vj_gf2_npc_golyat_damage_friendly"):GetBool(), true)
+	end
+end
+
+function ENT:CustomOnKilled() end ]]
