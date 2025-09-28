3ec4731ec4bc07fba97436d6f3cb6bbd81abd7a0
IBRS <56501720+IBRS-4Ever@users.noreply.github.com>
2025-09-28 19:48:33 +0800

    Subtitles.

--- lua/subtitles/girls_frontline_2/florence.lua
+++ lua/subtitles/girls_frontline_2/florence.lua
@@ -12,8 +12,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/alert1.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231853
 			["schinese"] = "来点小小的刺激怎么样？", 
+			["english"] = "How about a little excitement?",
+			["japanese"] = "ちょっと刺激を加えてみよっか？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -21,8 +23,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/alert2.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231854
 			["schinese"] = "别沉迷其中哦。", 
+			["english"] = "Don't lose yourself in this.",
+			["japanese"] = "夢中になっても知らないよ？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -30,8 +34,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/alert3.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 227661
 			["schinese"] = "全部——注射进去了！", 
+			["english"] = "Take it all, the whole thing!",
+			["japanese"] = "ぜ～んぶ、注射しとくね！",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -39,8 +45,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/alert4.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 227668
 			["schinese"] = "惩罚游戏，要开始了！", 
+			["english"] = "Time for the punishment game!",
+			["japanese"] = "罰ゲームの時間だよ！",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -48,8 +56,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/die1.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231859
 			["schinese"] = "这样才有挑战性嘛，指挥官。", 
+			["english"] = "That's what makes it challenging, Commander.",
+			["japanese"] = "難しい方がやりがいがあるでしょ？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -57,8 +67,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/follow1.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231846
 			["schinese"] = "啊，指挥官！我想到了一个新玩法，要一起试试吗？", 
+			["english"] = "Ah, Commander! I just thought of a new game we could play together. Want to try it out?",
+			["japanese"] = "ああ、指揮官。いいこと思いついたんだけど、一緒にやってみない？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -66,8 +78,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/follow2.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231847
 			["schinese"] = "哈喽，指挥官！芙洛伦前来报到，我会为你带来最刺激与愉悦的体验，所以，再兴奋一点吧？", 
+			["english"] = "Bonjour Commander! Florence reporting. I'll be delivering the most exhilarating and delightful experience you've ever had. So—come on, show a little more excitement, won't you?",
+			["japanese"] = "Bonjour！私が来たからには退屈なんかさせない。もっと喜んでもいいんだよ？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -75,8 +89,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/follow3.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231824
 			["schinese"] = "要玩些什么吗？", 
+			["english"] = "Would you like to play a game?",
+			["japanese"] = "面白いこと、してみない？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -84,9 +100,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/follow4.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231826
 			["schinese"] = "我上了！", 
-			["english"] = "I'm going!",
+			["english"] = "Here I come!",
+			["japanese"] = "行くよ！",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -94,8 +111,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/follow5.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231828
 			["schinese"] = "喔，来了来了！", 
+			["english"] = "Mmmnnn, here it comes, this is it!",
+			["japanese"] = "うん、これこれ！",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -103,8 +122,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/idle1.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231840
 			["schinese"] = "哎呀，指挥官，有哪里不舒服吗？不如让我来为你检查一下吧？", 
+			["english"] = "Oh my, you're not feeling well, Commander? Shall I give you a checkup?",
+			["japanese"] = "どこか具合でも悪いの？検査してあげよっか？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -112,8 +133,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/idle2.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231841
 			["schinese"] = "我明白的啦！无聊的时间总是令人难以忍耐，指挥官，要不要试试我手中的这支药剂，只要把它注入你的体内……你就会舒服很多哦？", 
+			["english"] = "I've got it! People feel uncomfortable when they're bored, so why don't you try out this new drug I came up with? I'll put it all... into you and you'll feel so much better, no?",
+			["japanese"] = "退屈な時間って長く感じるよね、分かるよ。この新薬を試してみない？注射すると、ソッコー気分が良くなるよ？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -121,8 +144,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/idle3.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231842
 			["schinese"] = "调配药剂时，总是有着无限未知的可能。就是这种感觉……啊……真令人着迷。", 
+			["english"] = "Formulating new pharmaceuticals always entails limitless unknown possibilities. And that feeling is... ah... it's bliss.",
+			["japanese"] = "薬を調合する時の感覚って病みつきになっちゃう……何ができるか分からないのって、素敵じゃない？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -130,8 +155,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/idle4.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231843
 			["schinese"] = "唔哇！指挥官，这么突然地靠近我，还真是吓了我一跳呢，哈哈哈。", 
+			["english"] = "Aiiieee? Oh, it's you, Commander. That felt good, though.",
+			["japanese"] = "ひゃっ！なんだ指揮官か。いいね、今のは良かったよ！",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -139,8 +166,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/idle5.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231844
 			["schinese"] = "指挥官，下次再一起去游乐园吧！我还想再体验一次呢，和指挥官一起以最快的速度，一次又一次地冲向最高点！", 
+			["english"] = "Commander, let's go to an amusement park again! I want to race towards the peak of sensation, over and over again, with you!",
+			["japanese"] = "今度また一緒に遊園地にいかない？前よりもっともっと楽しい時間を過ごそうよ！",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -148,8 +177,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/killed_enemy3.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231858
 			["schinese"] = "理所当然的结果，不过，还是有些不够刺激呢。", 
+			["english"] = "Things turned out as expected. Still, they weren't quite exciting enough.",
+			["japanese"] = "当然の結果だけど、ちょっと物足りないかな。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -157,8 +188,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/player_sight1.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231832
 			["schinese"] = "嗯？指挥官，你对我很感兴趣嘛~", 
+			["english"] = "Hm? Commander, are you interested in me~",
+			["japanese"] = "なぁに、私に興味持っちゃった？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -166,8 +199,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/player_sight2.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231833
 			["schinese"] = "果然，指挥官总是能带给我意想不到的惊喜呢。", 
+			["english"] = "Just as I thought, you're always full of surprises, Commander.",
+			["japanese"] = "やっぱり指揮官と一緒にいると、刺激に困らないね。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -175,8 +210,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/player_sight3.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231834
 			["schinese"] = "诶？指挥官都听说过？哈哈哈，看来我很有名嘛。确实有种治疗手段，会令患者上瘾呢，指挥官也想试试吗？", 
+			["english"] = "Hm? You've heard the rumors about me? Hahaha, looks like I'm quite famous. It's true that there are certain therapies that leave the patient hooked, want to try them out, Commander?",
+			["japanese"] = "へぇ、私の噂、そんなに広まってたんだ。ところで、ちょ～っと面白い治療法があるんだけど、試してみない？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -184,8 +221,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/player_sight4.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231835
 			["schinese"] = "今晚的实验已经准备好了哦，指挥官，你露出这样的表情，只会让我更想继续下去哦。", 
+			["english"] = "The preparations for tonight's experiments are ready. Oh Commander, when you make that face, it just makes me want to carry them out~",
+			["japanese"] = "今夜の実験の準備が出来たよ。フフ、そんな顔したら、ますますシたくなっちゃうでしょ？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -193,8 +232,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/player_sight5.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231836
 			["schinese"] = "抱歉！指挥官，上次让你晕过去是我不对！你会惩罚我吗？我愿意——接受一切惩罚哦。", 
+			["english"] = "Sorry I made you pass out earlier, Commander. You'll punish me, right? I'll accept aaaaaany kind of punishment.",
+			["japanese"] = "この前は気絶させてごめんね。罰なら受けるよ？どんな罰でも……ね。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -202,8 +243,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/player_sight6.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231837
 			["schinese"] = "这下你终于相信了，我用的都是无害的试剂。至于那些小小的副作用，只是一种惊喜的调味品罢了。", 
+			["english"] = "Now you'll believe that all my formulations are harmless, I trust? As for all those little side effects... well, just think of them as a surprise to make things interesting.",
+			["japanese"] = "私が使ってる薬が無害なの、ようやく信じてくれた？副作用の1つや2つはサプライズのようなもの。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -211,8 +254,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/player_sight7.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231838
 			["schinese"] = "没有什么能比留在指挥官的身边更令我兴奋了，哈哈，你现在的神情，就让我食欲大开呢，指挥官~", 
+			["english"] = "I can't think of anything more exciting than keeping you company, Commander. Ah, seeing that look on your face awakens my appetite, Commander~",
+			["japanese"] = "あなたが傍にいてくれる以上に刺激的なことはないよ。そうそう、その顔が大好物なんだ～",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -220,8 +265,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/player_sight8.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231839
 			["schinese"] = "答应我吧，指挥官？一直和我玩这样的游戏，直到永远……不答应的话——我们还在实验中吧？我手中的动作，可没那么容易停下来哦？", 
+			["english"] = "Promise me, Commander? Promise that you'll keep playing games like this with me forever? If not... well, we're still in the middle of an experiment, aren't we? And I won't be able to stop my hand so easily, no?",
+			["japanese"] = "これからもずっと、私と遊ぶって約束してくれる？しなかったら……この実験、このまま続けちゃうよ？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -229,8 +276,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/unfollow1.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231827
 			["schinese"] = "选我吧？", 
+			["english"] = "You're picking me?",
+			["japanese"] = "私にする？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -238,8 +287,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/unfollow2.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231829
 			["schinese"] = "还要忍下去吗？", 
+			["english"] = "Oh, don't be a tease.",
+			["japanese"] = "じらさないで。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -247,8 +298,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/florence/unfollow3.wav",
 		subject = CharacterName,
-		text = { -- ID: 
+		text = { -- ID: 231830
 			["schinese"] = "我等不及了。", 
+			["english"] = "I can't hold it in any longer.",
+			["japanese"] = "もう我慢できない……！",
 		},
 		subjectcol = CharacterColor,
 	},
--- lua/subtitles/girls_frontline_2/lind.lua
+++ lua/subtitles/girls_frontline_2/lind.lua
@@ -12,8 +12,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/alert1.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
-			["schinese"] = "文明地解决吧。", 
+		text = { -- ID: 241632
+			["schinese"] = "文明地解决吧。",
+			["english"] = "Let's do this in a civilized way.",
+			["japanese"] = "スマートに終わらせよう。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -21,8 +23,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/alert2.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
-			["schinese"] = "真恶心。", 
+		text = { -- ID: 241633
+			["schinese"] = "真恶心。",
+			["english"] = "Ew, gross.",
+			["japanese"] = "きも。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -30,8 +34,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/alert3.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241658
 			["schinese"] = "我给你个痛快吧。", 
+			["english"] = "I'll put you to rest now.",
+			["japanese"] = "楽にしてあげる。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -39,8 +45,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/alert4.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241659
 			["schinese"] = "啧，麻烦死了。", 
+			["english"] = "Tch, what a pain.",
+			["japanese"] = "ちぇ、めんどくさ……",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -48,8 +56,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/alert5.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241665
 			["schinese"] = "恶心死了，给我永远地消失！", 
+			["english"] = "You make me sick! Disappear forever!",
+			["japanese"] = "きも……消えてなくなれ！",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -57,8 +67,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/alert6.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241630
 			["schinese"] = "我要来真的了。", 
+			["english"] = "Time to get serious.",
+			["japanese"] = "本気になろっか。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -66,8 +78,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/die1.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241664
 			["schinese"] = "或许我根本就不适合这份工作吧……干脆辞职算了。", 
+			["english"] = "I knew it, I'm not suited for this job... Maybe I should quit.",
+			["japanese"] = "やっぱ私には向いてなかった……",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -75,8 +89,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/die2.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241638
 			["schinese"] = "糖分不足……", 
+			["english"] = "Sugar levels... falling...",
+			["japanese"] = "糖質が、足りない……",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -84,9 +100,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/die3.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241656
 			["schinese"] = "令人绝望……", 
-			["english"] = "Hopeless...",
+			["english"] = "I am left in despair...",
+			["japanese"] = "うそでしょ……",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -94,8 +111,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/follow1.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241637
 			["schinese"] = "有话快说。", 
+			["english"] = "Get to the point.",
+			["japanese"] = "何が言いたいの？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -103,8 +122,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/follow2.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241635
 			["schinese"] = "来咯——", 
+			["english"] = "Coming—",
+			["japanese"] = "はーい……",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -112,8 +133,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/follow3.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241625
 			["schinese"] = "那种正经到有点恶心的自我介绍就免了吧，又不是第一天认识了……名字？叫我琳德就好。", 
+			["english"] = "Can we pass on those excessively formal introductions? They make me want to puke. Besides, it's not like this is our first time meeting anyway... My name? Just call me Lind.",
+			["japanese"] = "自己紹介はだるいから無し……名前？「リンド」でいい。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -121,8 +144,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/follow4.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241627
 			["schinese"] = "升职加薪我还是很欢迎的啦。", 
+			["english"] = "The pay increase is very welcome.",
+			["japanese"] = "給料が上がるのはいいことだ。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -130,9 +155,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/hit6.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241657
 			["schinese"] = "真野蛮……", 
-			["english"] = "Brutal...", 
+			["english"] = "How uncivilized...",
+			["japanese"] = "野蛮だな……",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -140,8 +166,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/idle1.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241655
 			["schinese"] = "摄入糖分过多会故障，但不摄入的话我现在立刻就会报废，所以还是吃吧……唔姆。", 
+			["english"] = "Consuming too much sugar will make me malfunction, but I'll scrap myself on the spot if I don't get any right now, so I guess I should have some... Nom.",
+			["japanese"] = "糖質の摂り過ぎは素体に良くないけど、糖質を摂らなければメンタルがやられる……なら摂った方がいいな。うん。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -149,8 +177,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/idle2.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241619
 			["schinese"] = "干嘛一直盯着我看……啧，我不擅长做这种事啦，去找别的人形吧。", 
+			["english"] = "Why are you staring at me? I'm not good with this kinda stuff, go find another Doll for it.",
+			["japanese"] = "なにジロジロ見てんの……2人っきりは苦手だから、こういう相手は他の人形に頼んでよ……",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -158,8 +188,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/idle3.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241620
 			["schinese"] = "为什么人形也会想这么多啊，我现在可是焦虑得要死了啊！呼……还好，还有糖可以吃。", 
+			["english"] = "Why do Dolls have to think about so much stuff, I'm getting so anxious that it's killing me... Meh, at least I have candy.",
+			["japanese"] = "人形なのにどうして糖質が足りないとイライラすんのさ……飴でも舐めとこ。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -167,8 +199,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/idle4.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241621
 			["schinese"] = "怎么了？如果心情不好的话，就试试这个棒棒糖吧，给。", 
+			["english"] = "What's wrong? If you're feeling down, maybe a lollipop would help. Here, have one.",
+			["japanese"] = "気分が沈んでる時はロリポップを舐めるといいよ。ほら。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -176,8 +210,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/idle5.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241622
 			["schinese"] = "昨晚的电台里，我可是听到了不少关于你的事情哦，来，给我讲讲，到底有多少是真的……", 
+			["english"] = "I heard a lot of stuff about you on the radio last night. Now, why don't you tell me exactly how much of that was true...",
+			["japanese"] = "ゆうべのラジオで、あんたの色んな噂が流れてたよ。どこまでが本当か聞かせて。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -185,8 +221,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/killed_enemy1.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241663
 			["schinese"] = "火力充足，这可是我为数不多的优点。", 
+			["english"] = "Overwhelming firepower is a strength of mine.",
+			["japanese"] = "火力の高さだけが取り柄だよ、私は。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -194,8 +232,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/killed_enemy2.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241660
 			["schinese"] = "我可是认真的。", 
+			["english"] = "I'm being serious here.",
+			["japanese"] = "私は本気だよ。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -203,9 +243,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/killed_enemy3.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241662
 			["schinese"] = "去死。", 
-			["english"] = "Go to hell.", 
+			["english"] = "Die.",
+			["japanese"] = "死ね。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -213,8 +254,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/killed_enemy4.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241661
 			["schinese"] = "糖分补充完毕。", 
+			["english"] = "Glucose replenished.",
+			["japanese"] = "糖質補給完了。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -222,8 +265,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/killed_enemy5.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241626
 			["schinese"] = "还行吧……", 
+			["english"] = "Eh, I guess it's passable...",
+			["japanese"] = "まあまあかな……",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -231,8 +276,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/killed_enemy6.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241629
 			["schinese"] = "我还挺强的哦？", 
+			["english"] = "I'm pretty strong, aren't I?",
+			["japanese"] = "私ってまあまあ強いよ？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -240,8 +287,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/killed_enemy7.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241628
 			["schinese"] = "你在期待些什么？", 
+			["english"] = "What exactly were you expecting to happen?",
+			["japanese"] = "何を期待してんの？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -249,8 +298,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/player_sight1.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241647
 			["schinese"] = "好吧……我承认，你给的糖确实还挺甜的。", 
+			["english"] = "I'll admit it... the candy you give is pretty sweet.",
+			["japanese"] = "あんたがくれた飴、甘かったよ……",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -258,8 +309,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/player_sight2.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241648
 			["schinese"] = "干嘛一直对我这么好，有点奇怪……你不会别有企图吧？", 
+			["english"] = "Why are you being so nice to me? It feels a little weird... You don't have something else in mind, do you?",
+			["japanese"] = "なんで私に良くしてくれんの……変なこと考えてないよね？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -267,8 +320,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/player_sight3.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
-			["schinese"] = "事先说好，我可不会说那种恶心的话哦，你可能要失望了。", 
+		text = { -- ID: 241649
+			["schinese"] = "事先说好，我可不会说那种恶心的话哦，你可能要失望了。",
+			["english"] = "I'll just get this out of the way first, I can't bring myself to say all that cringy stuff, so you might be disappointed.",
+			["japanese"] = "先に言っておくけど、あんたが期待するような喋り方は出来ないからね。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -276,8 +331,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/player_sight4.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241650
 			["schinese"] = "指挥官，你会听深夜电台吗？喔……不听啊，没什么，其实你可以试着……听一下。", 
+			["english"] = "Commander, do you listen to late night radio shows? I see, so you don't... Oh, I didn't mean anything by it... but maybe you could give it a try.",
+			["japanese"] = "あんたって深夜ラジオとか聞くの？そうなんだ……深い意味はないけど、聞いてみるのもいいと思う。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -285,8 +342,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/player_sight5.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241651
 			["schinese"] = "多少也注意一点自己的言行吧，指挥官，别再做会让我多想的事情了。", 
+			["english"] = "Please pay attention to what you say and do, Commander, and don't do stuff that'll make me misunderstand you.",
+			["japanese"] = "あんたもさ、ちょっとは言動に気をつけた方がいいよ……じゃないと私みたいに誤解するやつが出て来る。",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -294,8 +353,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/unfollow1.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241636
 			["schinese"] = "哦？", 
+			["english"] = "Hmm?",
+			["japanese"] = "ん？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -303,8 +364,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/unfollow2.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241631
 			["schinese"] = "要补充点糖分吗？", 
+			["english"] = "Need a sugar topup?",
+			["japanese"] = "糖質補給する？",
 		},
 		subjectcol = CharacterColor,
 	},
@@ -312,8 +375,10 @@ local GF2Subtitle = { -- add new tables inside here
 	{
 		snd = "vo/jp/lind/unfollow3.wav", 
 		subject = CharacterName, 
-		text = { -- ID: 
+		text = { -- ID: 241634
 			["schinese"] = "快选吧。", 
+			["english"] = "Hurry up and pick.",
+			["japanese"] = "早くして。",
 		},
 		subjectcol = CharacterColor,
 	},
